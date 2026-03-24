f0ed63880a6ce8644631f4a2a371d63f9dfc62d0
Timbitee Esayas <esayasadisu@yahoo.com>
2026-03-23 23:54:03 -0400

    order_details CRUD operations

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -182,3 +182,51 @@ def delete_one_recipe(recipe_id: int, db: Session = Depends(get_db)):
    return recipes.delete(db=db, recipe_id=recipe_id)
 
 
+#---------------------------------- ORDER DETAIL --------------------------------------
+
+
+@app.post("/order_details/", response_model=schemas.OrderDetail, tags=["Order_details"])
+def create_order_detail(order_detail: schemas.OrderDetailCreate, db: Session = Depends(get_db)):
+   return order_details.create(db=db, order_detail=order_detail)
+
+
+
+
+@app.get("/order_details/", response_model=list[schemas.OrderDetail], tags=["Order_details"])
+def read_order_details(db: Session = Depends(get_db)):
+   return order_details.read_all(db)
+
+
+
+
+@app.get("/order_details/{order_detail_id}", response_model=schemas.OrderDetail, tags=["Order_details"])
+def read_one_order_detail(order_detail_id: int, db: Session = Depends(get_db)):
+   order_detail = order_details.read_one(db, order_detail_id=order_detail_id)
+   if order_detail is None:
+       raise HTTPException(status_code=404, detail="User not found")
+   return order_detail
+
+
+
+
+@app.put("/order_details/{order_detail_id}", response_model=schemas.OrderDetail, tags=["Order_details"])
+def update_one_order_detail(order_detail_id: int, order_detail: schemas.OrderDetailUpdate, db: Session = Depends(get_db)):
+   order_detail_db = order_details.read_one(db, order_detail_id=order_detail_id)
+   if order_detail_db is None:
+       raise HTTPException(status_code=404, detail="User not found")
+   return order_details.update(db=db, order_detail=order_detail, order_detail_id=order_detail_id)
+
+
+
+
+@app.delete("/order_details/{order_detail_id}", tags=["Order_details"])
+def delete_one_order_detail(order_detail_id: int, db: Session = Depends(get_db)):
+   order_detail = order_details.read_one(db, order_detail_id=order_detail_id)
+   if order_detail is None:
+       raise HTTPException(status_code=404, detail="User not found")
+   return order_details.delete(db=db, order_detail_id=order_detail_id)
+
+
+
+
+
